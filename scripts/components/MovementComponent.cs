73565482f59d29fb7d4df61cb7062d94841bcdde
Deepsn <75620590+Deepsn@users.noreply.github.com>
2023-10-20 19:02:59 -0300

    chore: modify player's velocity instead of position

--- scripts/components/MovementComponent.cs
+++ scripts/components/MovementComponent.cs
@@ -4,33 +4,14 @@ namespace DungeonGame.scripts.components;
 
 public partial class MovementComponent : Node2D
 {
-	[Export] public Area2D Player;
+	[Export] public CharacterBody2D Player;
 	[Export] public float Speed = 400;
 
 	public override void _Process(double delta)
 	{
-		var velocity = Vector2.Zero;
-
-		if (Input.IsActionPressed("move_right"))
-		{
-			velocity.X += 1;
-		}
-
-		if (Input.IsActionPressed("move_left"))
-		{
-			velocity.X -= 1;
-
-		}
-
-		if (Input.IsActionPressed("move_up"))
-		{
-			velocity.Y -= 1;
-		}
-
-		if (Input.IsActionPressed("move_down"))
-		{
-			velocity.Y += 1;
-		}
+		var horizontal = Input.GetAxis("move_left", "move_right");
+		var vertical = Input.GetAxis("move_up", "move_down");
+		var velocity = new Vector2(horizontal, vertical);
 
 		var animatedSprite2D = Player.GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 
@@ -38,6 +19,7 @@ public partial class MovementComponent : Node2D
 		{
 			velocity = velocity.Normalized() * Speed;
 
+			animatedSprite2D.FlipH = velocity.X < 0;
 			animatedSprite2D.Animation = "run";
 			animatedSprite2D.Play();
 		}
@@ -46,9 +28,7 @@ public partial class MovementComponent : Node2D
 			animatedSprite2D.Pause();
 		}
 
-		animatedSprite2D.FlipH = velocity.X < 0;
-
-		GD.Print("velocity", velocity);
-		Player.Position += velocity * (float)delta;
+		Player.Velocity += velocity * (float)delta;
+		Player.MoveAndSlide();
 	}
 }
