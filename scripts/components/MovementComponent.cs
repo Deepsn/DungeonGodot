5d6c086b15b51b248eb242f2827e672d37e7da11
Deepsn <75620590+Deepsn@users.noreply.github.com>
2023-10-21 01:26:40 -0300

    feat: new movement component

--- scripts/components/MovementComponent.cs
+++ scripts/components/MovementComponent.cs
@@ -2,33 +2,24 @@ using Godot;
 
 namespace DungeonGame.scripts.components;
 
-public partial class MovementComponent : Node2D
+public partial class MovementComponent : Node
 {
-	[Export] public CharacterBody2D Player;
-	[Export] public float Speed = 400;
+	[Export] public RigidBody2D Entity;
+	[Export] public float Speed = 70;
+
+	public Vector2 Velocity;
 
 	public override void _Process(double delta)
 	{
 		var horizontal = Input.GetAxis("move_left", "move_right");
 		var vertical = Input.GetAxis("move_up", "move_down");
-		var velocity = new Vector2(horizontal, vertical);
+		Velocity = new Vector2(horizontal, vertical);
 
-		var animatedSprite2D = Player.GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-
-		if (velocity.Length() > 0)
+		if (Velocity.Length() > 0)
 		{
-			velocity = velocity.Normalized() * Speed;
-
-			animatedSprite2D.FlipH = velocity.X < 0;
-			animatedSprite2D.Animation = "run";
-			animatedSprite2D.Play();
-		}
-		else
-		{
-			animatedSprite2D.Pause();
+			Velocity = Velocity.Normalized() * Speed;
 		}
 
-		Player.Velocity += velocity * (float)delta;
-		Player.MoveAndSlide();
+		Entity.MoveAndCollide(Velocity * (float)delta);
 	}
 }
