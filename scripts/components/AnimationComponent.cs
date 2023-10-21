0475817bcdcaad3daa571c5025a45b7a275183e1
Deepsn <75620590+Deepsn@users.noreply.github.com>
2023-10-21 01:26:57 -0300

    feat: player animation component

---
+++ scripts/components/AnimationComponent.cs
@@ -0,0 +1,25 @@
+﻿using Godot;
+
+namespace DungeonGame.scripts.components;
+
+public partial class AnimationComponent : Node
+{
+    [Export] public AnimatedSprite2D AnimatedSprite2D;
+    [Export] public MovementComponent MovementComponent;
+
+    public override void _Process(double delta)
+    {
+        var velocity = MovementComponent.Velocity;
+
+        AnimatedSprite2D.FlipH = velocity.X < 0;
+        AnimatedSprite2D.Animation =
+            velocity.Length() > 0
+                ? "run"
+                : "idle";
+    }
+
+    public override void _Ready()
+    {
+        AnimatedSprite2D.Play();
+    }
+}
