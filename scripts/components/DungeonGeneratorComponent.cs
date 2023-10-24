d8f04062f3afbdb35847f6a836831782780b0d6f
Deepsn <75620590+Deepsn@users.noreply.github.com>
2023-10-24 16:28:12 -0300

    feat: fill background with void tile

--- scripts/components/DungeonGeneratorComponent.cs
+++ scripts/components/DungeonGeneratorComponent.cs
@@ -1,4 +1,5 @@
-﻿using Godot;
+﻿using System;
+using Godot;
 using Godot.Collections;
 
 namespace DungeonGame.scripts.components;
@@ -9,12 +10,35 @@ public partial class DungeonGeneratorComponent : Node
     [Export] public TileMap DungeonTileMap;
     [Export] public Vector2I MainRoomSize = new Vector2I(6, 6);
 
+    [Export] public int VoidLayer;
     [Export] public int GroundLayer = 1;
     [Export] public int AtlasSourceId;
 
+    private void GenerateVoidBackground()
+    {
+        var rectSize = GetViewport().GetVisibleRect().Size;
+        Array<Vector2I> cellsCoords = new();
+        var backgroundSize = new Vector2(
+            MainRoomSize.X * 2 + rectSize.X / 16,
+            MainRoomSize.Y * 2 + rectSize.Y / 16
+        );
+
+        for (var x = 0; x < backgroundSize.X; x++)
+        {
+            for (var y = 0; y < backgroundSize.Y; y++)
+            {
+                cellsCoords.Add(new Vector2I(
+                    (int)(x - (backgroundSize.X / 2)),
+                    (int)(y - (backgroundSize.Y / 2))
+                ));
+            }
+        }
+
+        DungeonTileMap.SetCellsTerrainConnect(VoidLayer, cellsCoords, 0, 0, false);
+    }
+
     private void GenerateMainRoom()
     {
-        Vector2I cellCoords = new(1, 1);
         Array<Vector2I> cellsCoords = new();
 
         for (var x = 0; x < MainRoomSize.X; x++)
@@ -25,12 +49,12 @@ public partial class DungeonGeneratorComponent : Node
             }
         }
 
-        DungeonTileMap.SetCellsTerrainConnect(GroundLayer, cellsCoords, 0, 1, false);
+        DungeonTileMap.SetCellsTerrainConnect(GroundLayer, cellsCoords, 0, 2, false);
     }
 
     public override void _Ready()
     {
-        GD.Print("Ready");
+        GenerateVoidBackground();
         GenerateMainRoom();
     }
 }
